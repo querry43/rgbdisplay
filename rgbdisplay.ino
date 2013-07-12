38e69a6e4b8ddc1227fe3a3f5e96d1c818c9ec48
Matt Harrington <matt.harrington@rentrak.com>
2013-07-11 18:23:55 -0700

    adding bug program, automatic switching between bug and tetris

--- rgbdisplay.ino
+++ rgbdisplay.ino
@@ -2,6 +2,7 @@
 #include "SPI.h"
 #include "display.h"
 #include "tetris.h"
+#include "bug.h"
 #include "utils.h"
 
 
@@ -12,8 +13,12 @@ const int clockPin = 10;
 const int powerPin = 11;
 const int power = 20;
 
+#define BUG_SPEED (100ul * 5)
+#define TETRIS_SPEED (1000ul * 60 * 10)
+
 DisplayWrapper display = DisplayWrapper(nLEDs, dataPin, clockPin, powerPin, power);
-Tetris tetris = Tetris();
+Tetris tetris;
+environment_t environment;
 
 void setup() {
   Serial.begin(9600);
@@ -22,7 +27,22 @@ void setup() {
   display.show();
 }
 
+long previousMillis = 0;
+const long interval = 1000 * 60 * 30;
+
 void loop() {
-  tetris.drawRandomTetrisGrid();
-  delay(1000ul * 60 * 10);
+  while (millis() - previousMillis < interval) {
+    tetris.drawRandomTetrisGrid();
+    delay(TETRIS_SPEED);
+  }
+
+  previousMillis = millis();
+
+  while (millis() - previousMillis < interval) {
+    environment.advance();
+    environment.show();
+    delay(BUG_SPEED);
+  }
+
+  previousMillis = millis();
 }
